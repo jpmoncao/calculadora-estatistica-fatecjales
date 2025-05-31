3cdeb1a65894a5d52d3672db90ad53e47bce8df6
jpmoncao <pedrohjoao44@gmail.com>
2025-05-30 22:07:19 -0300

    feat: basic structure

---
+++ src/components/common/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header className="flex justify-between items-center w-full h-20 px-12 border-b-2 border-primary text-primary-foreground">
+            <div className="flex items-end gap-1">
+                <h1 className="text-5xl font-bold text-primary">Estatística</h1>
+                <h2 className="text-sm font-semibold text-primary/60">3º Sistemas para Internet</h2>
+            </div>
+        </header>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+
+import Header from '@/components/common/header'
 import Home from '@/pages/home'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
+  <BrowserRouter>
+    <Header />
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+
+    <footer className='w-full h-8 bg-background border-t-2 border-primary text-primary text-center'>
+      Desenvolvido por <a className='text-primary/70 hover:underline hover:text-primary/50 transition-all' rel='noopener' href='https://github.com/arthur-risso' target='_blank'>Arthur Risso</a> e <a className='text-primary/70 hover:underline hover:text-primary/50 transition-all' rel='noopener' href='https://github.com/jpmoncao' target='_blank'>João Pedro Monção</a>
+    </footer>
+  </BrowserRouter>,
 )
+
