17a94ee5b37d90ca5486f40390d0e2500dd59a39
jpmoncao <pedrohjoao44@gmail.com>
2025-05-30 22:17:34 -0300

    refactor: font family of heading 2

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
         <header className="flex justify-between items-center w-full h-20 px-12 border-b-2 border-primary text-primary-foreground">
             <div className="flex items-end gap-1">
                 <h1 className="text-5xl font-bold text-primary">Estatística</h1>
-                <h2 className="text-sm font-semibold text-primary/60">3º Sistemas para Internet</h2>
+                <h2 className="font-math text-xs font-semibold text-primary/60 mb-1">3º Sistemas para Internet</h2>
             </div>
         </header>
     )
